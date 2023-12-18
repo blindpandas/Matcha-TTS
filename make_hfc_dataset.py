31ee4208b3c81dbcf1992d3d445d676e2c28d69f
mush42 <musharraf.omer@gmail.com>
2023-12-18 19:09:03 +0200

    Added a script to create hfc datasets

---
+++ make_hfc_dataset.py
@@ -0,0 +1,45 @@
+# coding: utf-8
+
+import os
+from pathlib import Path
+
+
+# Change this path if appropriate
+ROOT_PATH = Path("/mnt/f/vocos-train/hfc-female/hi-fi-captain/en-US/female/")
+# ----------
+WAV_PATH = ROOT_PATH.joinpath("wav")
+TEXT_PATH = ROOT_PATH.joinpath("wav")
+
+
+TRAIN_DATASET = []
+for ut_type in ["train_parallel", "train_non_parallel"]:
+    text_path = TEXT_PATH.joinpath(f"{ut_type}.txt")
+    wav_path = WAV_PATH.joinpath(ut_type)
+    for line in text_path.read_text(encoding="utf-8").splitlines():
+        file_stem, text = [c.strip() for c in line.split("|")]
+        filepath = os.fspath(
+            wav_path.joinpath(file_stem).with_suffix(".wav")
+        )
+        TRAIN_DATASET.append(f"{filepath} | {text}")
+
+ROOT_PATH.joinpath("train.txt").write_text(
+    "\n".join(TRAIN_DATASET),
+    encoding="utf-8",
+    newline="\n"
+)
+
+EVAL_DATASET = []
+eval_wav = WAV_PATH.joinpath("eval")
+eval_lines = TEXT_PATH.joinpath("eval.txt").read_text(encoding="utf-8").splitlines()
+for line in eval_lines:
+    file_stem, text = [c.strip() for c in line.split("|")]
+    filepath = os.fspath(
+        eval_wav.joinpath(file_stem).with_suffix(".wav")
+    )
+    EVAL_DATASET.append(f"{filepath} | {text}")
+
+ROOT_PATH.joinpath("eval.txt").write_text(
+    "\n".join(EVAL_DATASET),
+    encoding="utf-8",
+    newline="\n"
+)
